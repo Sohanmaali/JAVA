ce10c5d863964d27d1a74c7ea2bf45089ff61c7b
Sohan Un <sohanmaali4@gmail.com>
2023-06-05 17:51:28 +0530

    add new program and file

---
+++ Method/Nr_with_argu/p1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; 
+
+class Test
+{
+	public int add(int x,int y)
+	{
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Adition = "+(x+y));
+		return x+y;
+		/* System.out.println("Do you want to parform more operation");
+		char ch = sc.next().charAt(0);
+		if(ch=='y') this.sub(x,y);
+		*/
+	}
+	public void sub(int a,int b)
+	{
+		System.out.println("Subtraction = "+(a-b));
+	}
+	public static void main(String args[])
+	{
+		Test ob = new Test();
+		//int x=0;
+		int x = ob.add(4,3);
+		System.out.println("Addition = "+x);
+	}
+}
