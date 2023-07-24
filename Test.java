7c737eb4e01ec374e5bec279e2d830dd0954aa04
sohan maali <sohanmaali4@gmail.com>
2023-07-24 14:39:44 +0530

    file add

--- Test.java
+++ Test.java
@@ -10,14 +10,12 @@ public class Test {
 
     public static void main(String[] args) {
 		 Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
-            float x=b/n;
-            System.out.println((a+x)+" ");
+       char a[] = {'s','o','h','a','n'}
+	   String s = "";
+        for(char i:a){
+           s+=i;
         }
         in.close();
     }
 }
+
