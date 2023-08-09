935b92ce5f7ac1c0e48bfc3cadbf0e87f8076fec
sohan maali <sohanmaali4@gmail.com>
2023-08-09 14:38:42 +0530

    file add

---
+++ MT/bank.java
@@ -0,0 +1,50 @@
+//import java.lang.*;
+class Main	
+{  
+	public static void main(String args[])
+	{ Trancection1 tran = new Trancection1(100000.0);
+		MyThread myt1 = new MyThread(tran);
+		MyThread myt2 = new MyThread(tran);
+		MyThread myt3 = new MyThread(tran);
+		MyThread myt4 = new MyThread(tran);
+	}
+}  
+class MyThread extends Thread
+{
+	Trancection1 tran;
+	MyThread (){}
+	MyThread (Trancection1 tran)
+	{
+		this.tran = tran;
+		this.start();
+	}
+	public void run()
+	{
+		tran.withdrow(1000.0 ,this);
+	}
+}
+class Trancection1
+{  
+	double balance;
+	Trancection1(){}
+	Trancection1 (double balance) 
+	{
+		this.balance += balance;
+	}
+	public void withdrow (double amount, Thread ref)
+	{
+		/* while(balance>0)
+		{ */
+		if(amount<=balance)
+		{
+			balance-=amount;
+			System.out.println("Total amout = "+ref.getName()+ " = "+balance);
+			// System.out.println();
+		}
+		else
+		{
+			System.out.println("Insufisent Balance");
+		}
+	// }
+	}
+} 
--- Try.java
+++ Try.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-
+import java.io.*;
+/* 
 class Main {
 
 	public static void main(String[] args) {
@@ -22,4 +23,34 @@ class Main {
 		}
 		System.out.println("hy i am ");
 	}
+} */
+
+
+
+// class Main {
+
+	/* public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BufferedReader z = new BufferedReader(new InputStreamReader(System.in));
+		char ch = (char) z.read();
+		System.out.println(ch);
+	} 
+}*/
+	class abc // enter class name (here abc is class name)
+{
+    public static void main(String arg[])throws IOException 
+    {
+		//InputStreamReader a = new InputStreamReader(System.in);
+        BufferedReader z = new BufferedReader(new InputStreamReader(System.in));
+        char ch = (char)z.read();
+		System.out.println(ch);
+	
+	
+		// InputStreamReader ch1 = new InputStreamReader(System.in);
+		/* BufferedReader ch1 = new BufferedReader(new InputStreamReader(System.in));
+        char b = (char)ch1.read();
+		System.out.println(b); */
+		
+		System.out.println("hello world");
+    }
 }
