750401cfd877fb09711dc9f9777f7270775df24d
Sohan maali <sohanmaali4@gmail.com>
2023-08-31 21:28:36 +0530

    oracle file add

---
+++ NetBeanse/JavaApplication1/src/jdbc/oracle/First.java
@@ -0,0 +1,31 @@
+package jdbc.oracle;
+
+import java.sql.*;
+
+public class First {
+
+    public static void main(String args[]) throws Exception {
+
+//step1 load the driver class  
+        System.out.println("1");
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+
+//step2 create  the connection object  
+        Connection con = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:xe", "system", "tiger");
+        System.out.println("2");
+
+//step3 create the statement object  
+        Statement stmt = con.createStatement();
+
+//step4 execute query  
+        ResultSet rs = stmt.executeQuery("select * from Student");
+        while (rs.next()) {
+            System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getInt(3));
+        }
+        System.out.println(rs.next());
+
+//step5 close the connection object  
+        con.close();
+    }
+}
