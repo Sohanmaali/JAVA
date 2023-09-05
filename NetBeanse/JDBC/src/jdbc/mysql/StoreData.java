f5173267d97d3b9181cbf9c198482bb17c12db22
Sohan maali <sohanmaali4@gmail.com>
2023-09-05 16:56:35 +0530

    file add

--- NetBeanse/JDBC/src/jdbc/mysql/StoreData.java
+++ NetBeanse/JDBC/src/jdbc/mysql/StoreData.java
@@ -3,21 +3,21 @@ package jdbc.mysql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class StoreData {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        PreparedStatement st;
+        Statement st;
         Connection con;
-        String cmd = "insert into Student values(1002,'sohan','rajesh','rani',100)";
+        String cmd = "insert into Student values(1003,'mohan','rajesh','rani',52)";
         String path = "jdbc:mysql://localhost:3306/Infojava";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         con = DriverManager.getConnection(path, "root", "root");
-        st = con.prepareStatement(cmd);
-
-        int i = st.executeUpdate();
+        st = con.createStatement();
+        int i = st.executeUpdate(cmd);
+        //con.commit();
         if (i > 0) {
             System.out.println("Succes");
         } else {
--- NetBeanse/JDBC/src/jdbc/oracle/First.java
+++ NetBeanse/JDBC/src/jdbc/oracle/First.java
@@ -18,12 +18,13 @@ public class First {
             //step3 create the statement object
             Statement stmt = con.createStatement();
             //step4 execute query
-            ResultSet rs = stmt.executeQuery("select * from Student");
+            ResultSet rs = stmt.executeQuery("select * from student");
             while (rs.next()) {
                 System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getInt(3));
             }
             System.out.println(rs.next());
             //step5 close the connection object
+            con.close();
         }
     }
 }
