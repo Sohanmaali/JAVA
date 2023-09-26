6f82dcb735f921e1f1394d0ae1cb4adea95b6335
sohan maali <sohanmaali4@gmail.com>
2023-09-26 06:39:30 +0530

    Servlet file added

---
+++ Exception/ServletFirst/src/main/java/first/FirstServlet.java
@@ -0,0 +1,42 @@
+package first;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+public class FirstServlet extends HttpServlet{
+
+	
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println(" "+Math.pow(5, 3));
+            
+        }
+    }
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+  
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
---
+++ NetBeanse/Servlet/src/java/Serice.java
@@ -0,0 +1,33 @@
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Serice extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = res.getWriter()) {
+            out.println("hello sohan");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "short Descraption";
+    }
+}
