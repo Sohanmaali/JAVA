e716f9951251e780375712814a5ced1ba8e33539
Sohan maali <sohanmaali4@gmail.com>
2024-01-11 01:13:30 +0530

    file add

---
+++ String/Count_Words.java
@@ -0,0 +1,11 @@
+public class Count_Words {
+     public static void main(String[] args) {
+          String s = "i am sohan maali";
+          int c = 0;
+
+          for (String x : a) {
+               c++;
+          }
+          System.out.println(c);
+     }
+}
