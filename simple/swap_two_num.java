8337b59852a0049f5c48ecce9f55893992d0be00
Sohan maali <sohanmaali4@gmail.com>
2024-02-10 06:59:49 +0530

    add

--- simple/swap_two_num.java
+++ simple/swap_two_num.java
@@ -1,24 +1,29 @@
 
-
 //swap two num without using third vareable and arethmeticla operator
 
 import java.util.Scanner;
-class SwapNum
-{
-	public static void main(String[]args)                                  
-	{
-		int a,b;
+
+class SwapNum {
+	public static void main(String[] args) {
+		int a, b;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter any two number");
 		a = sc.nextInt();
 		b = sc.nextInt();
-		System.out.println("Before swaping\nA = "+a+"\nB = "+b);
-		
-		//bitwise operator
-		a=a^b;
-		b=a^b;
-		a=a^b;
-		System.out.println("After swaping\nA = "+a+"\nB = "+b);
+		System.out.println("Before swaping\nA = " + a + "\nB = " + b);
+
+		// bitwise operator
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+		System.out.println("After swaping\nA = " + a + "\nB = " + b);
+
+		// Arithmetic operator
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println("After swaping\nA = " + a + "\nB = " + b);
+		sc.close();
 	}
 }
 
