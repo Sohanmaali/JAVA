c3299c80d0e3baa64c545ba319c5cb3ff8493c67
Sohan maali <sohanmaali4@gmail.com>
2024-02-21 13:21:40 +0530

    File Add

--- Loop/For/pattern/Practis/Pattern_56.java
+++ Loop/For/pattern/Practis/Pattern_56.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 /*
 **********
@@ -7,6 +6,8 @@ import java.util.Scanner;
 **      **
 *        *
 */
+import java.util.Scanner;
+
 public class Pattern_56 {
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
