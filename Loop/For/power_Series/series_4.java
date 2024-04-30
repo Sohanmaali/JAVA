e9ee8bc8e3f883772e1d433ab762237c67608602
Sohan Maali <sohanmaali4@gmail.com>
2024-04-30 21:09:38 +0530

    add

--- Loop/For/power_Series/series_4.java
+++ Loop/For/power_Series/series_4.java
@@ -1,23 +1,22 @@
-//WAP to x^1/1 + x^2/2 + x^3/3 + x^4/4 + x^5/5  =  17.066668
 
+//WAP to x^1/1 + x^2/2 + x^3/3 + x^4/4 + x^5/5  =  36.75
+//3 4
 import java.util.Scanner;
-class Series_4
-{
-	public static void main(String[]args)
-	{
+
+public class Series_4 {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter any two num ");
 		int x = sc.nextInt();
 		int y = sc.nextInt();
-		float sum = 0,power= 1;
-		for(int i = 1; i<=y; i++)
-		{
-			power *=x;
-			sum+=power/i;
-			if(i>1)
+		float sum = 0, power = 1;
+		for (int i = 1; i <= y; i++) {
+			power *= x;
+			sum += power / i;
+			if (i > 1)
 				System.out.print(" + ");
-			System.out.print(x+"^"+i+"/"+i);
+			System.out.print(x + "^" + i + "/" + i);
 		}
-		System.out.print(" = "+sum);
+		System.out.print(" = " + sum);
 	}
 }
